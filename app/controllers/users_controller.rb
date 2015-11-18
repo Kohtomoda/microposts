04724f6fd46a4d24fc24f7783ba88a4cbbd20450
kohtomoda <info@oj9.co.jp>
2015-11-18 16:57:05 +0000

    edit and update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+  
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'プロフィールを更新しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
   
   def show
   @user = User.find(params[:id])
@@ -23,4 +37,10 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+end
+
+private
+
+def set_user
+    @user = User.find(params[:id])
 end

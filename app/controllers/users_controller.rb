cf9caf93ea29df90f58252d202961289b4c2028c
kohtomoda <info@oj9.co.jp>
2015-11-22 11:17:15 +0000

    user_edit_controlle

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
   
   def edit
   end
@@ -35,7 +36,7 @@ class UsersController < ApplicationController
   private
   
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :area, :intro)
   end
 end
 
@@ -43,4 +44,9 @@ private
 
 def set_user
     @user = User.find(params[:id])
+end
+
+# セッションユーザーがユーザーと等しくなれけばroot_path
+def correct_user
+    redirect_to(root_url) unless @user == current_user
 end

576b06bff224ccab7f64605bf2c7027549ca7d08
kohtomoda <info@oj9.co.jp>
2015-12-05 16:55:59 +0000

    heroku-update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,8 +17,7 @@ class UsersController < ApplicationController
   
   def show
   @user = User.find(params[:id])
-  @microposts = @user.microposts.order(created_at: :desc)
-  @microposted = Micropost.page(params[:page]).order(created_at: :desc)
+  @microposts = @user.microposts.order(created_at: :desc).page(params[:page]).per(6).order(created_at: :desc)
   end
   
   def following
